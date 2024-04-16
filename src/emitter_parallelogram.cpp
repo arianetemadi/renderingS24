76139549e1f388ad16780ee5af9f868e1d4b128d
Arian Etemadi <arietemadi@gmail.com>
2024-04-16 19:42:18 +0200

    Direct lighting implemented with parallelogram light.

--- src/emitter_parallelogram.cpp
+++ src/emitter_parallelogram.cpp
@@ -5,7 +5,7 @@ NORI_NAMESPACE_BEGIN
 class ParallelogramEmitter : public Emitter {
 public:
     ParallelogramEmitter(const PropertyList &props) {
-
+        radiance = props.getColor("radiance");
     }
         
     EmitterRecord sample(const Point3f &ref,
@@ -13,9 +13,21 @@ public:
 
     float pdf(const EmitterParams &lRec) const {};
 
-    Color3f eval(const EmitterParams &lRec) const {};
+    Color3f eval(const EmitterParams &lRec) const { return radiance; };
+
+    Color3f getRadiance() { return radiance; }
     
-    std::string toString() const { return "ParallelogramEmitter[]"; }
+    std::string toString() const { 
+        return tfm::format(
+            "ParallelogramEmitter[\n"
+            "  radiance = %s\n"
+            "]",
+            radiance.toString()
+        );
+    }
+
+private:
+    Color3f radiance;
 };
 
 NORI_REGISTER_CLASS(ParallelogramEmitter, "parallelogram_emitter");
