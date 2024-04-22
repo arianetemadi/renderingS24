e105ea9e55a8152e1a6c468d9e38927e76e036d9
Arian Etemadi <arietemadi@gmail.com>
2024-04-22 17:58:11 +0200

    Code refactored. Added comments.

--- src/path_tracer_recursive.cpp
+++ src/path_tracer_recursive.cpp
@@ -12,7 +12,7 @@ public:
     PathTracerRecursiveIntegrator(const PropertyList &props) {
         russian_roulette = props.getBoolean("rr", false);
 
-        // Default: iterative
+        // The default is iterative, since it is more efficient
         iterative = props.getBoolean("iterative", true);
     }
 
@@ -35,10 +35,12 @@ private:
     int rr_min_bounces = 4;
     double rr_prob = 0.7;
 
-    // Recursive or iterative implementation,
-    // default: iterative
+    // Recursive or iterative implementation
     bool iterative;
 
+    /* Recursive version.
+       Can be selected by setting the corresponding parameter in the
+       scene file. */
     Color3f Li_recursive(const Scene *scene, Sampler *sampler, const Ray3f &ray, int bounce_cnt) const {
         /* Return black if reached the maximum number of bounces, only when no RR */
         if (!russian_roulette && bounce_cnt >= max_bounces) {
@@ -83,6 +85,9 @@ private:
                 + emitted_radiance;
     }
 
+    /* Iterative version.
+       This is default.
+       It is more efficient compared to recursive. */
     Color3f Li_iterative(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
         Color3f color(0.f);
         Color3f throughput(1.f);
@@ -90,15 +95,18 @@ private:
         Ray3f ray_copy(ray);
 
         while (true) {
+            /* stop if reached the maximum number of bounces, only when no RR */
             if (!russian_roulette && bounce_cnt >= max_bounces) {
                 break;
             }
 
+            /* Find the surface that is visible in the requested direction */
             Intersection its;
             if (!scene->rayIntersect(ray_copy, its)) {
                 break;
             }
 
+            /* Add the emitted radiance */
             Color3f emitted_radiance(0.f);
             if (its.mesh->isEmitter()) {
                 emitted_radiance = its.mesh->getEmitter()->eval(EmitterParams());
@@ -121,15 +129,14 @@ private:
             sample_ray.mint = Epsilon;
             sample_ray.update();
 
-            /* Return the incoming direct radiance from this direction */
-            BSDFParams params {its.toLocal(sample_ray.d), -its.toLocal(ray_copy.d)};
-
             /* Update throughput */
+            BSDFParams params {its.toLocal(sample_ray.d), -its.toLocal(ray_copy.d)};
             throughput *= (sample_ray.d.dot(its.shFrame.n))
                         * its.mesh->getBSDF()->eval(params)
                         * 2 * M_PI
                         / kill_prob;
 
+            /* Bounce the ray */
             bounce_cnt++;
             ray_copy = Ray3f(sample_ray);
         }
