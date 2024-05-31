2de633b164093540b1d8d49266eb9c39881dc877
Arian Etemadi <arietemadi@gmail.com>
2024-05-31 21:49:39 +0200

    Next event estimation implemented.

--- src/path_tracer_recursive.cpp
+++ src/path_tracer_recursive.cpp
@@ -14,14 +14,16 @@ public:
 
         // The default is iterative, since it is more efficient
         iterative = props.getBoolean("iterative", true);
+        
+        nee = props.getBoolean("nee", false);
     }
 
     Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
-        if (iterative) {  // Default
-            return Li_iterative(scene, sampler, ray);
-        } else {
+        // if (iterative) {  // Default
+        //     return Li_iterative(scene, sampler, ray);
+        // } else {
             return Li_recursive(scene, sampler, ray, 0);
-        }
+        // }
     }
     
     std::string toString() const { return "PathTracerRecursiveIntegrator[]"; }
@@ -37,6 +39,9 @@ private:
 
     // Recursive or iterative implementation
     bool iterative;
+
+    // Next Event Estimation (NEE)
+    bool nee;
 
     /* Recursive version.
        Can be selected by setting the corresponding parameter in the
@@ -54,8 +59,8 @@ private:
         }
 
         /* If directly hit light, add the emitted radiance */
-        Color3f emitted_radiance(0.f);
-        if (its.mesh->isEmitter()) {
+        Color3f emitted_radiance(0.0f);
+        if (its.mesh->isEmitter() && !(nee && bounce_cnt > 0)) {
             emitted_radiance = its.mesh->getEmitter()->eval(EmitterParams());
         }
 
@@ -68,6 +73,46 @@ private:
             }
         }
 
+        Color3f color;
+        if (nee) {
+            /* Choose an emitter uniformly at random */
+            uint32_t emitterInd = sampler->nextInt(scene->getEmitters().size());
+            const Emitter* emitter = scene->getEmitters()[emitterInd];
+            const Mesh* emitterMesh = emitter->getMesh();
+
+            /* Sample point uniformly at random on the emitter mesh */
+            std::pair<Point3f, Normal3f> sample = emitterMesh->samplePosition(sampler->next3D());
+            Vector3f dist = sample.first - its.p;
+
+            /* Check for occlusion */
+            Ray3f shadow_ray;
+            shadow_ray.o = its.p + Epsilon * its.shFrame.n;
+            shadow_ray.d = dist.normalized();
+            shadow_ray.mint = Epsilon;
+            // we want to find an intersection before the sample point,
+            // so shorten the maxt of the ray by a small amount
+            shadow_ray.maxt = dist.norm() - 100 * Epsilon;
+            shadow_ray.update();
+
+            /* Return black if occluded */
+            if (scene->rayIntersect(shadow_ray)) {
+                // return Color3f(0.0f);
+            }
+            else {
+
+            color = emitter->eval(EmitterParams());  // radiance of the emitter
+            /* In theory, the following two cosines should not be negative
+                at this point since there is no occlusion, although in practice
+                we need to catch some negatives */
+            color *= std::max(0.0f, shadow_ray.d.dot(its.shFrame.n));  // cosine
+            color *= std::max(0.0f, (-shadow_ray.d).dot(sample.second));  // emitter cosine
+            color /= dist.squaredNorm();  // emitter distance
+            BSDFParams params {its.toLocal(shadow_ray.d), -its.toLocal(ray.d)};
+            color *= its.mesh->getBSDF()->eval(params);  // BRDF
+            color /= emitterMesh->pdf() / scene->getEmitters().size();  // acts like multiplying by emitter surface area * #emitters
+        }}
+
+
         /* Sample BSDF */
         BSDFRecord bRec = 
             its.mesh->getBSDF()->sample(-its.toLocal(ray.d), sampler->next2D());
@@ -78,6 +123,14 @@ private:
         sample_ray.d = its.toWorld(bRec.params.wo);
         sample_ray.mint = Epsilon;
         sample_ray.update();
+
+        if (nee) {
+            // return color / kill_prob + emitted_radiance;
+            return (Li_recursive(scene, sampler, sample_ray, bounce_cnt + 1)
+                    * bRec.value
+                    + color) / kill_prob
+                    + emitted_radiance;
+        }
 
         /* Return the incoming direct radiance from this direction */
         return Li_recursive(scene, sampler, sample_ray, bounce_cnt + 1)
